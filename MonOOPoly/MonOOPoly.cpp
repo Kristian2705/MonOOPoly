7b6806e32252aabc457dedd1aa1d07bab9f5af01
Kristian2705 <k_petrov27@abv.bg>
2025-06-08 23:56:03 +0300

    Initial push

--- MonOOPoly/MonOOPoly.cpp
+++ MonOOPoly/MonOOPoly.cpp
@@ -1,9 +1,6 @@
-// MonOOPoly.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" << std::endl;
 }
